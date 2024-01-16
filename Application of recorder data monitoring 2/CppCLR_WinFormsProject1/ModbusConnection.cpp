9956a665ae1aaa4d44647140e5f27f0c49ceae10
AlexeyGrechishnikov <114112066+AlexeyGrechishnikov@users.noreply.github.com>
2024-01-16 17:14:46 +0300

    Add files via upload

--- Application of recorder data monitoring 2/CppCLR_WinFormsProject1/ModbusConnection.cpp
+++ Application of recorder data monitoring 2/CppCLR_WinFormsProject1/ModbusConnection.cpp
@@ -11,7 +11,7 @@ class ModbusConnection
 {
 public:
 	ModbusConnection(const char* ip, int port);
-	void modbus_read();
+	void modbus_read(int device_number);
 
 private:
 	modbus_t* ctx = NULL;
@@ -20,6 +20,7 @@ private:
 	int error;
 	std::list<std::string> notes;
 	uint16_t data[8001];
+	// Маски для обработки данных
 	uint16_t mask_month = 0b0000000000001111;
 	uint16_t mask_day = 0b0000000111110000;
 	uint16_t mask_hour = 0b1111111000000000;
@@ -44,13 +45,16 @@ ModbusConnection::ModbusConnection(const char* ip, int port)
 	}
 }
 
-void ModbusConnection::modbus_read()
+void ModbusConnection::modbus_read(int device_number) // Считывание по MODBUS TCP/IP
 {
-	error = modbus_read_registers(ctx, address, amount, data);
+	error = modbus_read_registers(ctx, address, amount, data); // Считывание непосредственно с контроллера 
 	int min, sec, msec, hour, day, month, code;
 	std::string date;
 	std::string time;
-	for (int i = 0; i < 8000; i += 5)
+	std::string note;
+	std::string full_note;
+	Device* dev=new Device();
+	for (int i = 0; i < 8000; i += 5)  // Обработка полученных данных
 	{
 		month = data[i + 1] & mask_month;
 		day = (data[i + 1] & mask_day) >> 4;
@@ -61,7 +65,25 @@ void ModbusConnection::modbus_read()
 		time = std::to_string(hour) + ":" + std::to_string(min) + ":" + std::to_string(sec) + "." + std::to_string(msec);
 		date = std::to_string(day) + "." + std::to_string(month);
 		code = data[i + 4];
-		
+		switch (device_number)
+		{
+		case 1:
+			note = dev->codes_cell.at(code);
+		case 2:
+			note = dev->codes_aggregate.at(code);
+		case 3:
+			note = dev->codes_control2.at(code);
+		case 4:
+			note = dev->codes_control3_tp4.at(code);
+		case 5:
+			note = dev->codes_control3_tp41.at(code);
+		case 6:
+			note = dev->codes_control3_tp16.at(code);
+		default:
+			break;
+		}
+
+
 	}
 }
 
