f76d1ee8aaf17325e950bd3281a82067cb265d3e
AlexeyGrechishnikov <114112066+AlexeyGrechishnikov@users.noreply.github.com>
2024-01-17 15:59:26 +0300

    Add files via upload

--- Application of recorder data monitoring/Application of recorder data monitoring/Device.cpp
+++ Application of recorder data monitoring/Application of recorder data monitoring/Device.cpp
@@ -2,20 +2,22 @@
 #include "Device.h"
 
 
-Device::Device(int dev_num, const char* ip, int port, int substation_number, std::string name)
+Device::Device(int dev_num, const char* ip, int port, int substation_number, std::string name, int district)
 {
 	this->ip = ip;
 	this->device_number = dev_num;
 	this->port = port;
 	this->device_name = name;
 	this->substation_number = substation_number;
+	this->district = district;
 }
 
-Device::Device(int dev_num, const char* ip, int substation_number, std::string name)
+Device::Device(int dev_num, const char* ip, int substation_number, std::string name, int district)
 {
 	this->ip = ip;
 	this->device_number = dev_num;
 	this->port = 502;
 	this->device_name = name;
 	this->substation_number = substation_number;
+	this->district = district;
 }
--- Application of recorder data monitoring/Application of recorder data monitoring/Device.h
+++ Application of recorder data monitoring/Application of recorder data monitoring/Device.h
@@ -6,12 +6,13 @@
 class Device
 {
 public:
-	Device(int dev_num,const char* ip,int port, int substation_number, std::string name);
-	Device(int dev_num, const char* ip, int substation_number, std::string name);
+	Device(int dev_num,const char* ip,int port, int substation_number, std::string name, int district);
+	Device(int dev_num, const char* ip, int substation_number, std::string name, int district);
 	int device_number;
 	std::string device_name;
 	int substation_number;
 	const char* ip;
 	int port;
+	int district;
 	std::list<std::string> notes;
 };
